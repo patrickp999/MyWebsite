a67f0f3a093be72e0f71fdf700608d3001743e78
Patrick <patrick.puga@epic.gov>
2019-09-15 20:00:39 -0600

    initial commit -m initial commint

---
+++ src/config/index.js
@@ -0,0 +1,62 @@
+module.exports = {
+    siteTitle: 'Patrick Puga | Software Engineer',
+    siteDescription: 'Patrick Puga is a Software Engineer based in El Paso, TX with experience developing enterprise applications throughout the full software lifecycle.',
+    siteKeywords:
+    'Patrick Puga, Patrick, Puga, ppuga999, software engineer, front-end engineer, backend engineer, web developer, javascript, full stack engineer, enterprise',
+    siteUrl: 'https://patrickpuga.com',
+    siteLanguage: 'en_US',
+    // googleAnalyticsID: '', TODO Add later
+    // googleVerification: '', 
+    name: 'Patrick Puga',
+    location: 'El Paso, TX',
+    email: 'ppuga999@gmail.com',
+    // github: '',
+    // twitterHandle: '',
+    socialMedia: [
+        {
+          name: 'Github',
+          url: '',
+        },
+        {
+          name: 'Linkedin',
+          url: '',
+        },
+        {
+          name: 'Codepen',
+          url: '',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+        },
+        {
+          name: 'Twitter',
+          url: '',
+        },
+      ],
+
+    navLinks: [
+      {
+        name: 'About',
+        url: '#about',
+      },
+      {
+        name: 'Experience',
+        url: '#jobs',
+      },
+      {
+        name: 'Work',
+        url: '#projects',
+      },
+      {
+        name: 'Contact',
+        url: '#contact',
+      },
+    ],
+
+    navHeight: 100,
+    lightBlueColor: '#2E9CCA',
+    BlueColor: '#29648A',
+    lightBlueBackgroundColor: '#464866',
+    darkBlueBackgroundColor: '#25274D',
+}
