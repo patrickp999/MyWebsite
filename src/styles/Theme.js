6c1428924084be899cfde39ee0f06d713a7614f3
Patrick <patrick.puga@epic.gov>
2020-05-17 16:44:17 -0600

    Fonts added

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,9 +1,29 @@
 const theme = {
-  colors: {},
+  colors: {
+    red: '#fd4345',
+    navyBlue: '#263547',
+    white: '#fdfdfd',
+    gray: '737373',
+    black: '#000000',
+  },
 
-  fonts: {},
+  fonts: {
+    Calibre:
+      'Calibre, San Francisco, SF Pro Text, -apple-system, system-ui, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif',
+    SFMono:
+      'SF Mono, Fira Code, Fira Mono, Roboto Mono, Lucida Console, Monaco, monospace',
+  },
 
-  fontSizes: {},
+  fontSizes: {
+    xsmall: '12px',
+    smallish: '13px',
+    small: '14px',
+    medium: '16px',
+    large: '18px',
+    xlarge: '20px',
+    xxlarge: '22px',
+    h3: '32px',
+  },
 };
 
 export default theme;
