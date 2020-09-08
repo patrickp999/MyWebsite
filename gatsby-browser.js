0052fde2319a8ad6d6dff2e8a5de04612075ef3e
Patrick Puga <patrick.puga@epic.gov>
2020-09-07 22:18:36 -0600

    scroll fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,18 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-exports.shouldUpdateScroll = ({
-    routerProps: { location },
-    getSavedScrollPosition
-  }) => {
-    const { pathname } = location
-    // list of routes for the scroll-to-top-hook
-    const scrollToTopRoutes = ['/']
-    // if the new route is part of the list above, scroll to top (0, 0)
-    if (scrollToTopRoutes.indexOf(pathname) !== -1) {
-      window.scrollTo(0, 0)
-    }
-  
-    return false
-  }
