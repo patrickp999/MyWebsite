98752c40c107febfbada9b0d828cb2a58a569fab
Patrick Puga <patrick.puga@epic.gov>
2020-09-22 23:14:18 -0600

    Fixed OG image

--- src/components/head.js
+++ src/components/head.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Helmet from "react-helmet";
-import ogImage from "@images/og.png";
-import config from "@config";
+import React from 'react';
+import Helmet from 'react-helmet';
+// import ogImage from '@images/og.png';
+import config from '@config';
 
 const Head = ({ metadata }) => (
   <Helmet>
@@ -17,14 +17,17 @@ const Head = ({ metadata }) => (
     <meta property='og:type' content='website' />
     <meta property='og:url' content={metadata.siteUrl} />
     <meta property='og:site_name' content={config.siteTitle} />
-    <meta property='og:image' content={`${metadata.siteUrl}${ogImage}`} />
+    <meta
+      property='og:image'
+      content={`${metadata.siteUrl}${config.siteImage}`}
+    />
     <meta property='og:image:width' content='1200' />
     <meta property='og:image:height' content='630' />
     <meta property='og:image:type' content='image/png' />
     <meta property='og:locale' content={config.siteLanguage} />
     <meta itemProp='name' content={metadata.title} />
     <meta itemProp='description' content={metadata.description} />
-    <meta itemProp='image' content={`${metadata.siteUrl}${ogImage}`} />
+    <meta itemProp='image' content={`${metadata.siteUrl}${config.siteImage}`} />
     {/* <meta name='twitter:card' content='summary_large_image' />
     <meta name='twitter:url' content={config.siteUrl} />
     <meta name='twitter:site' content={config.twitterHandle} />
